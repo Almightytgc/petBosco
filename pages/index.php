782ebfc4cb202accf1072aec9caaa7ecfaa0a70f
ineedoritos <cgon991@gmail.com>
2023-02-25 13:50:27 -0600

    m quiero matar no m acuerdo d nada sexo

--- pages/index.php
+++ pages/index.php
@@ -5,11 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../assets/images/favicon.png">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/home.css">
     
     <title>PetBosco</title>
 </head>
 <body>
-    <h1>OLA</h1>
+    <ul class="header">
+        <li class="header--item">PetBosco</li>
+        <li class="header--item">Iniciar Sesión</li>
+        <li class="header--item">Regístrate</li>
+    </ul>
+
+    <img src="../assets/svg/waves.svg" alt="waves">
 </body>
 </html>
