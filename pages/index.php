26eb0e64df63627ade3bffae0e966f1ed5a46f16
ineedoritos <cgon991@gmail.com>
2023-02-25 15:38:37 -0600

    idk ya m aburri y pasé toda la tarde

--- pages/index.php
+++ pages/index.php
@@ -4,18 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="../assets/images/favicon.png">
-    <link rel="stylesheet" href="../css/home.css">
+    <link rel="stylesheet" href="../assets/css/home.css">
     
     <title>PetBosco</title>
 </head>
 <body>
-    <ul class="header">
-        <li class="header--item">PetBosco</li>
-        <li class="header--item">Iniciar Sesión</li>
-        <li class="header--item">Regístrate</li>
-    </ul>
+    
+<div class="banner"></div>
 
-    <img src="../assets/svg/waves.svg" alt="waves">
+    
 </body>
 </html>
