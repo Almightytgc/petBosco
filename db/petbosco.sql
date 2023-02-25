33d52437000e5440d49a01b48f3720588ccbfc15
Almightytgc <crisostomoj63@gmail.com>
2023-02-24 18:21:05 -0600

    base datos actualizada 24 febrero

--- db/petbosco.sql
+++ db/petbosco.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-02-2023 a las 03:56:42
+-- Tiempo de generación: 25-02-2023 a las 01:19:43
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -24,11 +24,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `cita`
+-- Estructura de tabla para la tabla `citas`
 --
 
-CREATE TABLE `cita` (
-  `id_cita` int(11) NOT NULL,
+CREATE TABLE `citas` (
+  `id_cita` int(20) NOT NULL,
   `fechaHora` datetime NOT NULL,
   `motivo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -36,17 +36,28 @@ CREATE TABLE `cita` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `dueño`
+-- Estructura de tabla para la tabla `cliente`
 --
 
-CREATE TABLE `dueño` (
+CREATE TABLE `cliente` (
   `id_cliente` int(20) NOT NULL,
-  `nombres` varchar(70) NOT NULL,
-  `apellidos` varchar(70) NOT NULL,
+  `nombres` varchar(255) NOT NULL,
+  `apellidos` varchar(255) NOT NULL,
   `fechaNac` date NOT NULL,
-  `direccion` text NOT NULL,
-  `num_telefonico` int(10) NOT NULL,
-  `dui` int(11) NOT NULL
+  `dui` int(9) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `num_telefonico` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `expediente`
+--
+
+CREATE TABLE `expediente` (
+  `id_expediente` int(20) NOT NULL,
+  `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -56,15 +67,27 @@ CREATE TABLE `dueño` (
 --
 
 CREATE TABLE `mascota` (
-  `id_mascota` int(20) NOT NULL,
-  `apodo_masc` varchar(60) NOT NULL,
-  `especie` varchar(60) NOT NULL,
-  `raza` varchar(60) NOT NULL,
+  `id_mascota` int(11) NOT NULL,
+  `apodo` varchar(100) NOT NULL,
+  `especie` varchar(100) NOT NULL,
+  `raza` varchar(100) NOT NULL,
   `color` varchar(60) NOT NULL,
-  `fechaNac` date NOT NULL,
   `altura` double NOT NULL,
   `peso` double NOT NULL,
-  `condicion_masc` varchar(255) NOT NULL
+  `condición_mascota` varchar(255) NOT NULL,
+  `fechaNac` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pago`
+--
+
+CREATE TABLE `pago` (
+  `id_pago` int(20) NOT NULL,
+  `fecha` date NOT NULL,
+  `montoTotal` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -74,7 +97,7 @@ CREATE TABLE `mascota` (
 --
 
 CREATE TABLE `reportemedico` (
-  `id_reporte` int(11) NOT NULL,
+  `id_reporte` int(20) NOT NULL,
   `chequeoGeneral` varchar(255) NOT NULL,
   `medicamento` varchar(255) NOT NULL,
   `tratamiento` varchar(255) NOT NULL
@@ -87,12 +110,12 @@ CREATE TABLE `reportemedico` (
 --
 
 CREATE TABLE `veterinario` (
-  `id_vet` int(20) NOT NULL,
+  `id_veterinario` int(20) NOT NULL,
   `nombres` varchar(255) NOT NULL,
   `apellidos` varchar(255) NOT NULL,
   `fechaNac` date NOT NULL,
   `especialidad` varchar(255) NOT NULL,
-  `num_telefonico` int(10) NOT NULL
+  `num_telefonico` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -100,22 +123,22 @@ CREATE TABLE `veterinario` (
 --
 
 --
--- Indices de la tabla `cita`
+-- Indices de la tabla `expediente`
 --
-ALTER TABLE `cita`
-  ADD PRIMARY KEY (`id_cita`);
-
---
--- Indices de la tabla `dueño`
---
-ALTER TABLE `dueño`
-  ADD PRIMARY KEY (`id_cliente`);
+ALTER TABLE `expediente`
+  ADD PRIMARY KEY (`id_expediente`);
 
 --
 -- Indices de la tabla `mascota`
 --
 ALTER TABLE `mascota`
   ADD PRIMARY KEY (`id_mascota`);
+
+--
+-- Indices de la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD PRIMARY KEY (`id_pago`);
 
 --
 -- Indices de la tabla `reportemedico`
@@ -127,41 +150,41 @@ ALTER TABLE `reportemedico`
 -- Indices de la tabla `veterinario`
 --
 ALTER TABLE `veterinario`
-  ADD PRIMARY KEY (`id_vet`);
+  ADD PRIMARY KEY (`id_veterinario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `cita`
+-- AUTO_INCREMENT de la tabla `expediente`
 --
-ALTER TABLE `cita`
-  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `dueño`
---
-ALTER TABLE `dueño`
-  MODIFY `id_cliente` int(20) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `expediente`
+  MODIFY `id_expediente` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `mascota`
 --
 ALTER TABLE `mascota`
-  MODIFY `id_mascota` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_mascota` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pago`
+--
+ALTER TABLE `pago`
+  MODIFY `id_pago` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `reportemedico`
 --
 ALTER TABLE `reportemedico`
-  MODIFY `id_reporte` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_reporte` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `veterinario`
 --
 ALTER TABLE `veterinario`
-  MODIFY `id_vet` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_veterinario` int(20) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
