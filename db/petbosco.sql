804775aa0a9f2a48514993a9e7a7a88627aae7a7
ineedoritos <cgon991@gmail.com>
2023-02-25 21:05:13 -0600

    uptade relationships db
    añañin aña

--- db/petbosco.sql
+++ db/petbosco.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 25-02-2023 a las 01:19:43
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Servidor: localhost:3306
+-- Tiempo de generación: 26-02-2023 a las 03:04:12
+-- Versión del servidor: 5.7.33
+-- Versión de PHP: 8.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `citas` (
   `id_cita` int(20) NOT NULL,
   `fechaHora` datetime NOT NULL,
-  `motivo` varchar(255) NOT NULL
+  `motivo` varchar(255) NOT NULL,
+  `fk_cliente` int(20) NOT NULL,
+  `fk_veterinario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -46,8 +48,22 @@ CREATE TABLE `cliente` (
   `fechaNac` date NOT NULL,
   `dui` int(9) NOT NULL,
   `direccion` varchar(255) NOT NULL,
-  `num_telefonico` int(10) NOT NULL
+  `num_telefonico` int(10) NOT NULL,
+  `fk_pago` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clinicavet`
+--
+
+CREATE TABLE `clinicavet` (
+  `id_clinica` int(20) NOT NULL,
+  `num_telefonico` varchar(20) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `fk_veterinario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -75,7 +91,8 @@ CREATE TABLE `mascota` (
   `altura` double NOT NULL,
   `peso` double NOT NULL,
   `condición_mascota` varchar(255) NOT NULL,
-  `fechaNac` date NOT NULL
+  `fechaNac` date NOT NULL,
+  `fk_cliente` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -100,7 +117,8 @@ CREATE TABLE `reportemedico` (
   `id_reporte` int(20) NOT NULL,
   `chequeoGeneral` varchar(255) NOT NULL,
   `medicamento` varchar(255) NOT NULL,
-  `tratamiento` varchar(255) NOT NULL
+  `tratamiento` varchar(255) NOT NULL,
+  `fk_expediente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -115,12 +133,36 @@ CREATE TABLE `veterinario` (
   `apellidos` varchar(255) NOT NULL,
   `fechaNac` date NOT NULL,
   `especialidad` varchar(255) NOT NULL,
-  `num_telefonico` int(11) NOT NULL
+  `num_telefonico` int(11) NOT NULL,
+  `fk_mascota` int(20) NOT NULL,
+  `fk_reporteMedico` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`id_cita`),
+  ADD KEY `fk_cliente` (`fk_cliente`),
+  ADD KEY `fk_veterinario` (`fk_veterinario`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD KEY `fk_pago` (`fk_pago`);
+
+--
+-- Indices de la tabla `clinicavet`
+--
+ALTER TABLE `clinicavet`
+  ADD PRIMARY KEY (`id_clinica`),
+  ADD KEY `fk_veterinario` (`fk_veterinario`);
 
 --
 -- Indices de la tabla `expediente`
@@ -132,7 +174,9 @@ ALTER TABLE `expediente`
 -- Indices de la tabla `mascota`
 --
 ALTER TABLE `mascota`
-  ADD PRIMARY KEY (`id_mascota`);
+  ADD PRIMARY KEY (`id_mascota`),
+  ADD KEY `fk_clienteMascota` (`fk_cliente`),
+  ADD KEY `fk_cliente` (`fk_cliente`);
 
 --
 -- Indices de la tabla `pago`
@@ -144,17 +188,38 @@ ALTER TABLE `pago`
 -- Indices de la tabla `reportemedico`
 --
 ALTER TABLE `reportemedico`
-  ADD PRIMARY KEY (`id_reporte`);
+  ADD PRIMARY KEY (`id_reporte`),
+  ADD KEY `fk_reporteMedico` (`fk_expediente`);
 
 --
 -- Indices de la tabla `veterinario`
 --
 ALTER TABLE `veterinario`
-  ADD PRIMARY KEY (`id_veterinario`);
+  ADD PRIMARY KEY (`id_veterinario`),
+  ADD KEY `fk_mascota` (`fk_mascota`),
+  ADD KEY `fk_reporteMedico` (`fk_reporteMedico`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `citas`
+--
+ALTER TABLE `citas`
+  MODIFY `id_cita` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `clinicavet`
+--
+ALTER TABLE `clinicavet`
+  MODIFY `id_clinica` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `expediente`
@@ -185,6 +250,48 @@ ALTER TABLE `reportemedico`
 --
 ALTER TABLE `veterinario`
   MODIFY `id_veterinario` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD CONSTRAINT `fk_clienteCitas` FOREIGN KEY (`fk_cliente`) REFERENCES `cliente` (`id_cliente`),
+  ADD CONSTRAINT `fk_veterinarioCitas` FOREIGN KEY (`fk_veterinario`) REFERENCES `veterinario` (`id_veterinario`);
+
+--
+-- Filtros para la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD CONSTRAINT `fk_pago` FOREIGN KEY (`fk_pago`) REFERENCES `pago` (`id_pago`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `clinicavet`
+--
+ALTER TABLE `clinicavet`
+  ADD CONSTRAINT `fk_veterinario` FOREIGN KEY (`fk_veterinario`) REFERENCES `veterinario` (`id_veterinario`);
+
+--
+-- Filtros para la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  ADD CONSTRAINT `fk_cliente` FOREIGN KEY (`fk_cliente`) REFERENCES `cliente` (`id_cliente`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `reportemedico`
+--
+ALTER TABLE `reportemedico`
+  ADD CONSTRAINT `fk_expediente` FOREIGN KEY (`fk_expediente`) REFERENCES `expediente` (`id_expediente`);
+
+--
+-- Filtros para la tabla `veterinario`
+--
+ALTER TABLE `veterinario`
+  ADD CONSTRAINT `fk_mascota` FOREIGN KEY (`fk_mascota`) REFERENCES `mascota` (`id_mascota`),
+  ADD CONSTRAINT `fk_reporteMedico` FOREIGN KEY (`fk_reporteMedico`) REFERENCES `reportemedico` (`id_reporte`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
